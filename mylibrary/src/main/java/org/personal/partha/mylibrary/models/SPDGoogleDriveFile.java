418ae7d0822c6d8625cd36d7a6b343c44c617e08
shapartha <65087693+shapartha@users.noreply.github.com>
2021-02-06 10:33:23 +0530

    Release 2.0.8.2 -> Google Drive Classes

--- mylibrary/src/main/java/org/personal/partha/mylibrary/models/SPDGoogleDriveFile.java
+++ mylibrary/src/main/java/org/personal/partha/mylibrary/models/SPDGoogleDriveFile.java
@@ -1,7 +1,20 @@
 package org.personal.partha.mylibrary.models;
 
+import com.google.api.client.json.GenericJson;
 import com.google.api.services.drive.model.File;
 
-public class SPDGoogleDriveFile {
-    public static File MyFile;
+public class SPDGoogleDriveFile extends GenericJson {
+    File myFile;
+
+    public SPDGoogleDriveFile(File myFile) {
+        this.myFile = myFile;
+    }
+
+    public File getMyFile() {
+        return myFile;
+    }
+
+    public void setMyFile(File myFile) {
+        this.myFile = myFile;
+    }
 }
